063f9f80bd6d438550708fc4160a28fc2774b360
artemka <artyom266.17@list.ru>
2026-04-29 19:33:35 +0700

    feat: add streamlit utils and service factories

---
+++ streamlit_app/utils.py
@@ -0,0 +1,60 @@
+import asyncio
+import streamlit as st
+
+from src.infrastructure.database.database_setup import async_session
+from src.infrastructure.database.repositories.raw_material import RawMaterialRepository, RawMaterialStockRepository
+from src.infrastructure.database.repositories.package import PackageRepository, PackageStockRepository
+from src.infrastructure.database.repositories.product import ProductRepository, PackagingRepository, ProductStockRepository
+from src.infrastructure.database.repositories.craft import CraftRepository
+from src.infrastructure.database.repositories.defect_rate import DefectRateRepository
+from src.infrastructure.database.repositories.customer import CustomerRepository
+from src.application.raw_material import RawMaterialService
+from src.application.package import PackageService
+from src.application.product import ProductService
+from src.application.craft import CraftService
+from src.application.defect_rate import DefectRateService
+from src.application.customer import CustomerService
+
+
+def sync(coro):
+    return asyncio.run(coro)
+
+
+@st.cache_resource
+def get_raw_material_service() -> RawMaterialService:
+    return RawMaterialService(
+        RawMaterialRepository(async_session),
+        RawMaterialStockRepository(async_session),
+    )
+
+
+@st.cache_resource
+def get_package_service() -> PackageService:
+    return PackageService(
+        PackageRepository(async_session),
+        PackageStockRepository(async_session),
+    )
+
+
+@st.cache_resource
+def get_product_service() -> ProductService:
+    return ProductService(
+        ProductRepository(async_session),
+        PackagingRepository(async_session),
+        ProductStockRepository(async_session),
+    )
+
+
+@st.cache_resource
+def get_craft_service() -> CraftService:
+    return CraftService(CraftRepository(async_session))
+
+
+@st.cache_resource
+def get_defect_rate_service() -> DefectRateService:
+    return DefectRateService(DefectRateRepository(async_session))
+
+
+@st.cache_resource
+def get_customer_service() -> CustomerService:
+    return CustomerService(CustomerRepository(async_session))
